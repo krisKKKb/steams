215422a6f6bfc27f131c99ebc4d88e2130948379
kriskkkb <krisburk577@gmail.com>
2020-10-29 15:13:54 +0200

    pede

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,129 @@
 <template>
-  <img src="https://store.cloudflare.steamstatic.com/public/images//gift/steamcards_promo_02.png" alt="" srcset="">
-  <ul class="uppercase text-sm text-gray-500">Kinkekaardid</ul>
-  <ul class="uppercase text-sm text-gray-500 mt-6">Soovitused
-      <li class="normal-case flex items-center text-blue-500"><span class="bg-icons h-4 w-4 inline-block mr-2"></span>Sõprade järgi</li>
-      <li class="normal-case flex items-center text-blue-500"><span class="bg-icons bg-curators h-4 w-4 inline-block mr-2"></span>Kuraatorid</li>
-      <li class="normal-case flex items-center text-blue-500"><span class="bg-icons bg-tags h-4 w-4 inline-block mr-2"></span>Tagid</li>
+  <img
+    src="https://store.cloudflare.steamstatic.com/public/images//gift/steamcards_promo_02.png"
+    alt=""
+    srcset=""
+  />
+  <ul class="uppercase text-sm text-gray-500">
+    Kinkekaardid
   </ul>
-  <ul class="uppercase text-sm text-gray-500 mt-6">Avastammise järjekord
-      <li class="normal-case flex items-center text-blue-500"><span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span>Soovitused</li>
-      <li class="normal-case flex items-center text-blue-500"><span class="bg-icons bg-new h-4 w-4 inline-block mr-2"></span>Uued</li>
+  <ul class="uppercase text-sm text-gray-500 mt-6">
+    Soovitused
+    <li class="normal-case flex items-center text-blue-500">
+      <span class="bg-icons h-4 w-4 inline-block mr-2"></span>Sõprade järgi
+    </li>
+    <li class="normal-case flex items-center text-blue-500">
+      <span class="bg-icons bg-curators h-4 w-4 inline-block mr-2"></span
+      >Kuraatorid
+    </li>
+    <li class="normal-case flex items-center text-blue-500">
+      <span class="bg-icons bg-tags h-4 w-4 inline-block mr-2"></span>Tagid
+    </li>
   </ul>
-  <ul class="uppercase text-sm text-gray-500 mt-6">Kategooriad</ul>
-  <ul class="uppercase text-sm text-gray-500 mt-6">Žanrid</ul>
+  <ul class="uppercase text-sm text-gray-500 mt-6">
+    Avastammise järjekord
+    <li class="normal-case flex items-center text-blue-500">
+      <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+      >Soovitused
+    </li>
+    <li class="normal-case flex items-center text-blue-500">
+      <span class="bg-icons bg-new h-4 w-4 inline-block mr-2"></span>Uued
+    </li>
+  </ul>
+  <ul class="uppercase text-sm text-gray-500 mt-6">
+    Kategooriad
+  </ul>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Enim müünud
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Uued väljalasked
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Tulemas
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Spetsiaalsed
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Virtuaal Reaalsus
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Puldi Sõbralikud
+  </li>
+  <ul class="uppercase text-sm text-gray-500 mt-6">
+    Žanrid
+  </ul>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Tasuta Mängud
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Varajane Juurdepääs
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Märul
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Seiklus
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Vabaaja
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Iseseisvad Mängud
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Mitmikmängud
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Võidusõit
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >RPG
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Simulatsioon
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Sport
+  </li>
+  <li class="normal-case flex items-center text-blue-500">
+    <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+    >Strateegia
+  </li>
+  <ul class="uppercase text-sm text-gray-500 mt-6">
+    Viimati Vaadatud
+    <li class="normal-case flex items-center text-blue-500">
+      <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+      >Cyberpunk 2077
+    </li>
+    <li class="normal-case flex items-center text-blue-500">
+      <span class="bg-icons bg-reccomendations h-4 w-4 inline-block mr-2"></span
+      >Outlast
+    </li>
+  </ul>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
